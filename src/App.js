d3c41a86bbb7c51fcec8410c1d951ca893de3ddf
Kay Kostadinov <kaloyan.kostadinov@anakatech.com>
2020-12-08 15:33:33 +0200

    Created base structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react'
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Editor from './components/Editor';
@@ -7,11 +9,27 @@ import Theme from './components/Theme';
 
 
 function App() {
+  const [data, setData] = useState({
+    name: '',
+    title: '',
+    phone: '',
+    fax: '',
+    website: '',
+    address: '',
+    social: {
+        fb: '',
+        ig: '',
+        linkedin: '',
+    }
+})
+
+console.log(data);
+
   return (
     <div className="ms-main">
       <Header/>
       <main>
-        <Editor />
+        <Editor data={data} setData={setData} />
 
       </main>
       <Footer />
